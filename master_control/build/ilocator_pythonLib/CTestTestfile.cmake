4482e271223c310165a0bd13a04fe57f70ecf9c7
Anders Christoffersen <ac7ex@virginia.edu>
2020-11-09 15:38:28 +0100

    Removed build folder
    Removed build folder for stateNode and rpiLightingNode

--- master_control/build/ilocator_pythonLib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anders/catkin_ws/src/ilocator_pythonLib
-# Build directory: /home/anders/catkin_ws/build/ilocator_pythonLib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- master_control/build/rpiLightingNode/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anders/catkin_ws/src/rpiLightingNode
-# Build directory: /home/anders/catkin_ws/build/rpiLightingNode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
