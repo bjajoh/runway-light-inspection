518a266d4e72f4f7b0fb10dc590f9a7fbffdc55f
Bjarne Johannsen <bjarne.johannsen@protonmail.com>
2020-11-18 19:33:43 +0100

    added newest updates

--- master_control/src/rpiLightingNode/rpiLightingNode.py
+++ master_control/src/rpiLightingNode/rpiLightingNode.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env/python
+#!/usr/bin/python3
+
 import rospy
 #These next two are for the LEDs, verify that i have the right ones.
-#import board
-#import neopixel #Assuming we can use this, I just went for something quick.
+import board
+import neopixel #Assuming we can use this, I just went for something quick.
 #Quick variable assignment for the pixels on the board. THIS WILL DEPEND ON THE
 #LED STRIP. It's a slightly more filled in placeholder I guess
 pixels = neopixel.NeoPixel(board.D18, 30)
-global incomingState = ''
+
+incomingState = None
+
 
 
 def stateCallback(data):
+    global incomingState
     incomingState = data.data
 
 def lightingNode():
+    global incomingState
     rospy.init_node('lighting')
     rospy.Subscriber("state", String,stateCallback)
     rate = rospy.Rate(20)
