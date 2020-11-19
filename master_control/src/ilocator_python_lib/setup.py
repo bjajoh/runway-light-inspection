efc45e145733e6a7548fec14af2e598bb79c018b
ac7ex <ac7ex@virginia.edu>
2020-11-19 18:01:26 +0100

    Update to rpi_lighting_node
    Updates to fix incorrect switch statement in rpi_lighting_node
    
    Also fixed naming conventions.

--- master_control/src/ilocator_python_lib/setup.py
+++ master_control/src/ilocator_python_lib/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages = ['ilocator_pythonLib'],
+    packages = ['ilocator_python_lib'],
     package_dir = {'': 'src'},
 )
 
