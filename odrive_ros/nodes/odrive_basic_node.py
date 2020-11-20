ce750e6a82e457cae860933b9bdbd271ec546a07
janprz <jasiek.przybyszewski@gmail.com>
2020-11-20 10:43:40 -0800

    Add Twist message publishing with encoder estimated velocities.

--- odrive_ros/nodes/odrive_basic_node.py
+++ odrive_ros/nodes/odrive_basic_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #import roslib; roslib.load_manifest('BINCADDY')
 import rospy
 #import roslib
@@ -20,6 +20,7 @@ import time
 import math
 import traceback
 import queue
+import numpy as np
 
 from odrive_interface import ODriveInterfaceAPI, ODriveFailure
 from odrive_interface import ChannelBrokenException, ChannelDamagedException
@@ -29,72 +30,103 @@ from odrive_simulator import ODriveInterfaceSimulator
 class ODriveNode(object):
 
 
-    def __init__(self):
+        def __init__(self):
 
-    	self.wheel_track = 0.4
-    	self.m_s_to_value = 1.0
-    	self.tyre_circumference = 0.2032
-    	self.vel_subscriber = rospy.Subscriber("/cmd_vel",Twist, self.cmd_vel_callback, queue_size=2)
-    	self.status_pub = rospy.Publisher('status', std_msgs.msg.String, queue_size=2)
-    	self.status = "disconnected"
-    	self.interface = ODriveInterfaceAPI()
-
-
-    def connect(self):
-    	result = self.interface.connect()
-    	result = self.interface.connect()
-    	self.status = "connected"
-    	self.status_pub.publish(("Connecting... result: {}".format(result)))
+            self.wheel_track = 0.4
+            self.m_s_to_value = 1.0
+            self.tyre_circumference = 0.2032
+            self.angular_vel_limit = 2.0
+            self.vel_subscriber = rospy.Subscriber("/cmd_vel",Twist, self.cmd_vel_callback, queue_size=2)
+            self.status_pub = rospy.Publisher('/odrive_basic_node/status', std_msgs.msg.String, queue_size=2)
+            self.encoder_pub = rospy.Publisher('/odrive_basic_node/twist_estimation',Twist, queue_size=10)
+            self.status = "disconnected"
+            self.interface = ODriveInterfaceAPI()
 
 
-    def engage(self):
-        result = self.interface.engage()
-        self.status_pub.publish("Engaging... result: {}".format(result))
+        def connect(self):
+            self.status_pub.publish("Connecting...")
+            result = self.interface.connect()
+            result = self.interface.connect()
+            if result == True:
+                self.status = "connected"
+            else:
+                self.status = "not connected"
+            self.status_pub.publish(("Connecting... result: {}".format(self.status)))
+            
 
 
-    def release(self):
-        self.interface.release()
-        self.status_pub.publish("Released")
-
-    def convert(self, forward, ccw):
-        angular_to_linear = ccw * (self.wheel_track/2.0) 
-        left_linear_val  = float((forward - angular_to_linear) * self.m_s_to_value)
-        right_linear_val = float((forward + angular_to_linear) * self.m_s_to_value)
-        print("Left: {}, Right: {}".format(left_linear_val,right_linear_val))
-        return left_linear_val,right_linear_val
+        def engage(self):
+                result = self.interface.engage()
+                self.status_pub.publish("Engaging... result: {}".format(result))
 
 
-    def cmd_vel_callback(self, msg):
-        left_linear_val, right_linear_val = self.convert(msg.linear.x, msg.angular.z)
-        if left_linear_val < 4 and right_linear_val < 4:
-            self.interface.drive(left_linear_val,right_linear_val)
-            self.status_pub.publish("Driving... velocities: {},{}".format(left_linear_val,right_linear_val))
+        def release(self):
+                self.interface.release()
+                self.status_pub.publish("Released")
+
+
+        def compute_estimated_twist(self, left_angular_vel, right_angular_vel):
+                message = Twist()
+                left_linear_vel = -1.0*left_angular_vel*self.tyre_circumference*2.0
+                right_linear_vel = right_angular_vel*self.tyre_circumference*2.0
+                angular_vel = (right_linear_vel-left_linear_vel)/self.wheel_track*2.0
+                linear_vel = (left_linear_vel+right_linear_vel)/2.0
+                message.linear.x = linear_vel
+                message.angular.z = angular_vel
+                return message
+                    
+
+        def convert(self, forward, ccw):
+                angular_to_linear = ccw * (self.wheel_track/2.0) 
+                left_angular_vel  = (forward - angular_to_linear/2.0) * self.m_s_to_value/self.tyre_circumference/2.0
+                right_angular_vel = (forward + angular_to_linear/2.0) * self.m_s_to_value/self.tyre_circumference/2.0
+                left_angular_vel = np.sign(left_angular_vel) * min(abs(left_angular_vel),self.angular_vel_limit)
+                right_angular_vel = np.sign(right_angular_vel) * min(abs(right_angular_vel),self.angular_vel_limit)
+                return left_angular_vel,right_angular_vel
+
+
+        def cmd_vel_callback(self, msg):
+                left_angular_vel, right_angular_vel = self.convert(msg.linear.x, msg.angular.z)
+                self.interface.drive(right_angular_vel,left_angular_vel)
+                self.status_pub.publish("Driving... velocities: {},{}".format(left_angular_vel,right_angular_vel))
+
+
+        def encoder_publisher_loop(self):
+            rate = rospy.Rate(10)
+            while not rospy.is_shutdown():
+                try:
+                    left_vel_estimate = self.interface.left_vel_estimate()
+                    right_vel_estimate = self.interface.right_vel_estimate()
+                    print("encoder estimations: {},{}".format(left_vel_estimate,right_vel_estimate))
+                    left_pos = str(self.interface.left_pos())
+                    right_pos = str(self.interface.right_pos())
+                    left_current = str(self.interface.left_current())
+                    right_current = str(self.interface.right_current())
+                    message = self.compute_estimated_twist(left_vel_estimate, right_vel_estimate)
+                    self.encoder_pub.publish(message)
+                    rate.sleep()
+                except rospy.ROSInterruptException:
+                    break
 
 
 
-odrive_node = None
-
-def stopEngines():
-	global odrive_node
-	release()
-
-def start_odrive():
+def stopEnginesAndRelease():
     global odrive_node
-    rospy.init_node('odrive')
-    odrive_node = ODriveNode()
-    odrive_node.connect()
-    odrive_node.engage()
-    # odrive_node.main_loop()
-    rospy.on_shutdown(stopEngines)	
-    rospy.spin() 	
-
-
-def release():
-    global odrive_node
+    odrive_node.interface.drive(0,0)
     odrive_node.release()
-    
+
+def start_odrive(odrive_node):
+        rospy.init_node('odrive')
+        odrive_node.connect()
+        odrive_node.engage()
+        odrive_node.encoder_publisher_loop()
+        # odrive_node.main_loop()
+        rospy.on_shutdown(stopEnginesAndRelease)
+        rospy.spin()    
+        
 if __name__ == '__main__':
-    try:
-        start_odrive()
-    except rospy.ROSInterruptException:
-        release()
+        try:
+                odrive_node = ODriveNode()
+                start_odrive(odrive_node)
+        except rospy.ROSInterruptException:
+                pass
